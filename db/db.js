21d1a592fee065e3986dd12f159bfc96328d18b1
annanay aggarwal <anni.agg2003@gmail.com>
2023-08-29 01:52:54 +0530

    commit

--- db/db.js
+++ db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = process.env.MONGO_URI_DEV;
+const MONGO_URI = process.env.MONGO_URI;
  //const MONGO_URI = 'mongodb://127.0.0.1:27017/portal-api';
 
 //console.log(MONGO_URI)
